21bca465a01a5d411a4a197ec14a5702276dd7ca
joedenby <jd744@kent.ac.uk>
2022-11-22 12:36:46 +0000

    Added AINode comments

--- Assets/Scripts/AIScripts/AINode.cs
+++ Assets/Scripts/AIScripts/AINode.cs
@@ -10,20 +10,46 @@ public class AINode : Node
         this.GUID = GUID;
     }
 
+    /**
+     * Calls on node activation.
+     * 
+     * @param AIBase
+     */
     public virtual void OnStart(AIBase npc) {
     }
 
+    /**
+     * Calls for every rendered frame node is active.
+     * 
+     * @param AIBase
+     */
     public virtual void OnUpdate(AIBase npc) { 
         
     }
 
+    /*
+     * Called on the last frame before leaving node.
+     * 
+     * @param AIBase
+     */
     public virtual void OnEnd(AIBase npc) { 
         
     }
 
+    /**
+     * Returns confidence of given nodes use.
+     * 
+     * @param AIBase
+     */
     public virtual float Weight(AIBase npc) => 0;
 
+    /**
+     * Returns if node should remain has chosen node for given frame.
+     * 
+     * @param AIBase
+     */
     public virtual bool Active(AIBase npc) => false;
+
 
     public virtual bool BoolResult(AIBase npc) => false;
 

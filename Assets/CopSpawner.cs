cf32efab8d65f90e1c685c4359074be8c99cc24b
joedenby <jd744@kent.ac.uk>
2023-02-27 14:24:47 +0000

    push

--- Assets/CopSpawner.cs
+++ Assets/CopSpawner.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 public class CopSpawner : MonoBehaviour
 {
     [SerializeField] private List<GameObject> activeUnits = new List<GameObject>();
+    [SerializeField] private Transform[] spawnPoints = new Transform[0]; 
 
     private async void FixedUpdate()
     {
         if (GameManager.WantedLevel == 0) return;
-
     }
 
 }

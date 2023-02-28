06c95481be49036a2c056d790b1de4d07c143d64
joedenby <dolefish@rocketmail.com>
2023-02-28 13:06:07 +0000

    TestRoom

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,11 +5,17 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float speed = 50;
     private Rigidbody rb => GetComponent<Rigidbody>();
 
-    private void FixedUpdate() => rb.velocity = transform.forward * speed;
+    private void Start() => rb.AddForce(Vector3.forward, ForceMode.Acceleration);
 
     private void OnCollisionEnter(Collision collision)
     {
-        gameObject.SetActive(false);
+        transform.GetChild(0).gameObject.SetActive(true);
         transform.localPosition = Vector3.zero;
+        Invoke("Disable", 1f);
+    }
+
+    private void Disable() {
+        gameObject.SetActive(false);
+        transform.GetChild(0).gameObject.SetActive(false);
     }
 }
